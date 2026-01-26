93a70ddb2d2f9079e498d883e16def3f9f5dd2d3
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-01-26 07:52:55 +0545

    User Model updated to append PostIds as an array

---
+++ src/models/user/user.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  phoneNumber?: string;
+  address?: string;
+  role: "user" | "moderator";
+  bio?: string;
+  avatar?: string;
+  followerCount: number;
+  followingCount: number;
+  postCount: number;
+  posts: mongoose.Types.ObjectId[];
+}
+
+const UserSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    password: { type: String, required: true, select: false },
+    phoneNumber: { type: String },
+    address: { type: String },
+    role: { type: String, enum: ["user", "moderator"], default: "user" },
+    bio: { type: String },
+    avatar: { type: String },
+    followerCount: { type: Number, default: 0 },
+    followingCount: { type: Number, default: 0 },
+    postCount: { type: Number, default: 0 },
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post", index: true }],
+  },
+  { timestamps: true },
+);
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
