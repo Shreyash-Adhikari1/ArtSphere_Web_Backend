e53d396d2938768e89d7d359ce60510eb2cdc9bd
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-01-26 08:01:18 +0545

    The Backend Application remains Functional until This Point

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import express, { Application } from 'express';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-import connectDB from './database/db';
-import userRouter from './routes/user.route';
-
-
-
+import express, { Application } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import connectDB from "./database/db";
+import cors from "cors";
+import userRouter from "./routes/user/user.route";
+import postRouter from "./routes/post/post.route";
+import followRouter from "./routes/follow/follow.route";
+import path from "path";
+import commentRouter from "./routes/post/comment.route";
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT;
+
+let corsOptions = {
+  origin: ["http://localhost:5000", "http://localhost:3000"],
+  // which url can access backend
+  // put your frontend domain/url here
+};
+// origin: "*", // yo sabai url lai access dinxa
+app.use(cors(corsOptions));
+
+app.use("/uploads", express.static(path.join(__dirname, "../uploads"))); // static file serving
 
 //database connection
 connectDB();
@@ -17,10 +29,16 @@ connectDB();
 // Middleware
 app.use(bodyParser.json());
 
-// Register Routes
-app.use('/api/user', userRouter); 
+// User Routes
+app.use("/api/user", userRouter);
 
+// Post Routes
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
+
+// Follow Route
+app.use("/api/follow", followRouter);
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server running at: http://localhost:${PORT}`);
+  console.log(`Server running at: http://localhost:${PORT}`);
 });
