c6503b2b3af8ff2c7078ac687a8ff40d26b45541
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-02-04 22:25:06 +0545

    Submission Repo Methods Defined || Nested Populate Query Implemented

--- src/features/submission/repository/submission.repository.ts
+++ src/features/submission/repository/submission.repository.ts
@@ -8,9 +8,17 @@ export interface ISubmissionRepository {
     postId: string,
   ): Promise<ISubmission | null>;
 
-  getAllSubmissions(): Promise<ISubmission[]>;
-  getSubmissionsToChallenge(challengeId: string): Promise<ISubmission[]>;
+  getAllSubmissions(skip: number, limit: number): Promise<ISubmission[]>;
+  getSubmissionsForChallenge(
+    challengeId: string,
+    skip: number,
+    limit: number,
+  ): Promise<ISubmission[]>;
   getSubmissionById(submissionId: string): Promise<ISubmission | null>;
+  getByChallengeAndSubmitter(
+    challengeId: string,
+    submitterId: string,
+  ): Promise<ISubmission | null>;
 
   deleteSubmission(submissionId: string): Promise<ISubmission | null>;
   deleteAllSubmissions(challengerId: string): Promise<ISubmission | null>;
@@ -26,19 +34,62 @@ export class SubmissionRepository implements ISubmissionRepository {
     const newSubmission = new SubmissionModel(challengeId, submitterId, post);
     return newSubmission.save();
   }
-  getAllSubmissions(): Promise<ISubmission[]> {
-    throw new Error("Method not implemented.");
+  async getAllSubmissions(
+    skip: number = 0,
+    limit: number = 10,
+  ): Promise<ISubmission[]> {
+    return await SubmissionModel.find()
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 })
+      .exec();
   }
-  getSubmissionsToChallenge(challengeId: string): Promise<ISubmission[]> {
-    throw new Error("Method not implemented.");
+  async getSubmissionsForChallenge(
+    challengeId: string,
+    skip: number = 0,
+    limit: number = 10,
+  ): Promise<ISubmission[]> {
+    return await SubmissionModel.find({ challengeId })
+      // Nested Populate Query Used
+      // something like this
+      // SubmittedPost---baata-->PostDetails---baata--->UserDetails
+      .populate({
+        path: "submittedPostId",
+        select: "media caption likeCount commentCount author",
+        populate: {
+          path: "author",
+          select: "_id username avatar",
+        },
+      })
+      // .populate(
+      //   "submittedPostId",
+      //   "author media caption likeCount commentCount",
+      // )
+      // .populate("submittedPostId.author", " _id username avatar ")
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .exec();
   }
-  getSubmissionById(submissionId: string): Promise<ISubmission | null> {
-    throw new Error("Method not implemented.");
+  async getSubmissionById(submissionId: string): Promise<ISubmission | null> {
+    return await SubmissionModel.findById(submissionId).populate({
+      path: "submittedPostId",
+      select: "media caption likeCount commentCount author",
+      populate: { path: "author", select: "_id username avatar" },
+    });
+  }
+  async getByChallengeAndSubmitter(
+    challengeId: string,
+    submitterId: string,
+  ): Promise<ISubmission | null> {
+    return await SubmissionModel.findOne({ challengeId, submitterId }).exec();
   }
   async deleteSubmission(submissionId: string): Promise<ISubmission | null> {
     return await SubmissionModel.findByIdAndDelete(submissionId).exec();
   }
-  deleteAllSubmissions(challengerId: string): Promise<ISubmission | null> {
+  async deleteAllSubmissions(
+    challengerId: string,
+  ): Promise<ISubmission | null> {
     throw new Error("Method not implemented.");
   }
 }
