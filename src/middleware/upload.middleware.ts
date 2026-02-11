890276e75fc56ed98d24976686892ca4f2621adf
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2026-02-11 11:49:12 +0545

    Adjusted Upload Middleware and Post Controller for testing

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -10,25 +10,30 @@ if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    console.log("Multer destination called", file.fieldname);
-    //
-    // const folderName = req.file?.fieldname; // profile-image // post-image
-    const folderName = file.fieldname; //doing this because gpt says req.file is undefined at this point, whatever that means
-    const uploadDir = path.join(__dirname, "../../uploads/" + folderName);
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir, { recursive: true });
-    }
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    console.log("Multer filename called", file.originalname);
-    const uniqueSuffix = uuidv4();
-    const extension = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${uniqueSuffix}${extension}`);
-  },
-});
+// Made such that test doesnt require dev file path
+// test can only access the memory bit
+const storage =
+  process.env.NODE_ENV === "test"
+    ? multer.memoryStorage() // a little conditional statement that decides if its test or dev
+    : multer.diskStorage({
+        destination: function (req, file, cb) {
+          console.log("Multer destination called", file.fieldname);
+          //
+          // const folderName = req.file?.fieldname; // profile-image // post-image
+          const folderName = file.fieldname; //doing this because gpt says req.file is undefined at this point, whatever that means
+          const uploadDir = path.join(__dirname, "../../uploads/" + folderName);
+          if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir, { recursive: true });
+          }
+          cb(null, uploadDir);
+        },
+        filename: function (req, file, cb) {
+          console.log("Multer filename called", file.originalname);
+          const uniqueSuffix = uuidv4();
+          const extension = path.extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${extension}`);
+        },
+      });
 
 const fileFilter = (
   req: Express.Request,
